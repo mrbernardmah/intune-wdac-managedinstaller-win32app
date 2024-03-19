853cd67d6ce064b202ce5d716d58552dd9b37dcc
Mr B <iTranc3@gmail.com>
2024-03-19 11:14:53 +1000

    Add files via upload

---
+++ Enable_ManagedInstaller_Policy.ps1
@@ -0,0 +1,5 @@
+﻿#Apply the App Locker policy for Managed Installer
+Set-AppLockerPolicy -XmlPolicy .\managedinstaller.xml -Merge -ErrorAction SilentlyContinue
+
+#Start the App Identity Service (required for AppLocker to function) and change startup to Automatic
+sc.exe config appidsvc start= auto
